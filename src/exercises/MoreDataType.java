7a48da58c63545866ecf592e67be3f9f5b582c5b
Michael Pu <michael.pu123@gmail.com>
2017-09-27 13:37:11 -0400

    updated more data type

--- src/exercises/MoreDataType.java
+++ src/exercises/MoreDataType.java
@@ -3,9 +3,23 @@ package exercises;
 import java.util.Scanner;
 
 public class MoreDataType {
+	
+	private static Scanner in;
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		in = new Scanner(System.in);
+		Question1();
+		Question2();
+		Question3();
+		Question4();
+		Question5();
+		Question6();
+		Question7();
+		Question8();
+		in.close();
+	}
+	
+	private static void Question1() {
 		
 		/* QUESTION 1
 		 * INPUT: N, positive integer, the amount of change that is due in cents
@@ -28,7 +42,9 @@ public class MoreDataType {
 		int P = cents;
 		
 		System.out.println("Return " + T + " toonies, " + L + " loonies, " + Q + " quarters, " + D + " dimes, " + N + " nickels, " + P + " pennies.");
+	}	
 	
+	private static void Question2() {
 		/*
 		 * QUESTION 2
 		 * INPUT: a positive number, the number of seconds the brick has traveled
@@ -40,6 +56,9 @@ public class MoreDataType {
 		in.nextLine();
 		double dis = ((double)1/2)*9.81*(S*S);
 		System.out.println("In " + S + " seconds, the brick travelled " + dis + " metres.");
+	}
+	
+	private static void Question3() {
 		
 		/*
 		 * QUESTION 3
@@ -50,6 +69,9 @@ public class MoreDataType {
 		System.out.print("Enter your name: ");
 		String name = in.nextLine();
 		System.out.println("Hello, " + name);
+	}
+	
+	private static void Question4() {
 		
 		/*
 		 * QUESTION 4
@@ -63,6 +85,9 @@ public class MoreDataType {
 			System.out.print(namePart.toUpperCase().charAt(0));
 		}
 		System.out.println();
+	}
+	
+	private static void Question5() {
 		
 		/*
 		 * QUESTION 5
@@ -71,8 +96,11 @@ public class MoreDataType {
 		 */
 		
 		System.out.print("Enter you full name: ");
-		fullName = in.nextLine().split(" ");
+		String[] fullName = in.nextLine().split(" ");
 		System.out.println(fullName[0] + " " + fullName[fullName.length-1]);
+	}
+	
+	private static void Question6() {
 		
 		/*
 		 * QUESTION 6
@@ -86,6 +114,9 @@ public class MoreDataType {
 			System.out.print(word.charAt(i));
 		}
 		System.out.println();
+	}
+	
+	public static void Question7() {
 		
 		/*
 		 * QUESTION 7
@@ -94,17 +125,19 @@ public class MoreDataType {
 		 */
 		
 		System.out.print("Enter the word: ");
-		word = in.nextLine();
+		String word = in.nextLine();
 		System.out.println(word.toUpperCase());
-		
+	}
+	
+	public static void Question8() {
 		/*
 		 * QUESTION 8
 		 * INPUT: A string of text, a word with an odd number of letters
 		 * OUTPUT: A string of text, the word with the middle character removed
 		 */
-		
+		 
 		System.out.print("Enter the word: ");
-		word = in.nextLine();
+		String word = in.nextLine();
 		int middle = word.length()/2;
 		System.out.println(word.substring(0, middle) + word.substring(middle+1));
 	}
