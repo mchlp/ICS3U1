218028dc270dae417d01f7f8b51936ec55990f14
Michael Pu <michael.pu123@gmail.com>
2017-10-20 14:53:19 -0400

    add file reading and writing

--- src/encryption/FileEncrypt.java
+++ src/encryption/FileEncrypt.java
@@ -1,22 +1,51 @@
 package encryption;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class FileEncrypt {
 
 	public static void main(String[] args) {
 
-		final String text = "hello world!";
-		final int move = 5;
-		int min = 97;
-		int max = 123;
+		String text = "hello world!";
+		final int move = 3;
+		final int min = 97;
+		final int max = 123;
 
-		for (int i = 0; i < text.length(); i++) {
-			char c = text.charAt(i);
-			if (c != ' ') {
-				int newIndex = ((int) c + move) >= max ? min + ((int) c + move) - max : (int) c + move;
-				System.out.print((char) newIndex);
-			} else {
-				System.out.print(" ");
+		File inFile = new File("src/encryption/in.txt");
+		Scanner in = null;
+		try {
+			in = new Scanner(inFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		File outFile = new File("src/encryption/out.txt");
+		PrintStream out = null;
+		try {
+			out = new PrintStream(outFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		while (in.hasNextLine()) {
+			text = in.nextLine();
+			System.out.println(text);
+			for (int i = 0; i < text.length(); i++) {
+				char c = text.charAt(i);
+				if (c != ' ') {
+					int newIndex = ((int) c + move) >= max ? min + ((int) c + move) - max : (int) c + move;
+					// System.out.print((char) newIndex);
+					out.append((char) newIndex);
+				} else {
+					// System.out.println(" ");
+					out.append(" ");
+				}
 			}
 		}
+		out.close();
+		System.out.println("Test");
 	}
 }
