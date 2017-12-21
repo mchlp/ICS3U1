85e8976386336bffa0035a2b6cd2bcec1afa4f79
Michael Pu <michael.pu123@gmail.com>
2017-12-21 11:21:28 -0500

    updated

--- src/intro/DataTypes.java
+++ src/intro/DataTypes.java
@@ -1,77 +1,4 @@
-<<<<<<< HEAD
-/*
- * Michael Pu
- * ICS3U1 - DataTypes
- * November 2017
- */
 
-package intro;
-
-import java.util.Scanner;
-
-public class DataTypes {
-
-	public static void main (String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		/*
-		 * QUESTION 1
-		 * INPUT: a number, N
-		 * OUTPUT: the cube of N
-		 */
-
-		System.out.print("Enter a number: ");
-		double N = in.nextInt();
-		System.out.println("The cube of " + N + " is " + Math.pow(N, 3));
-		
-		/*
-		 * QUESTION 2
-		 * INPUT: a positive integer, N
-		 * OUTPUT: the square root of N
-		 */
-
-		System.out.print("Enter a number: ");
-		N = in.nextInt();
-		System.out.println("The square root of " + N + " is " + Math.sqrt(N));
-		
-		/*
-		 * QUESTION 3
-		 * INPUT: integers a, b, c 
-		 * OUTPUT: the roots of the equation y = ax^2 + bx + c
-		 */
-
-		System.out.print("Enter a: ");
-		int a = in.nextInt();
-		System.out.print("Enter b: ");
-		int b = in.nextInt();
-		System.out.print("Enter c: ");
-		int c = in.nextInt();
-
-		double thingUnderRoot = Math.sqrt((b*b) - (4*a*c));
-		double x1 = ((b*-1) + thingUnderRoot)/(2*a);
-		double x2 = ((b*-1) - thingUnderRoot)/(2*a);
-
-		System.out.println("The roots are " + x1 + " and " + x2);
-		
-		/*
-		 * QUESTION 4
-		 * INPUT: the number of children, seniors, and adults
-		 * OUTPUT: the price of the tickets
-		 */
-
-		System.out.print("Enter number of children: ");
-		int children = in.nextInt();
-		System.out.print("Enter the number of seniors: ");
-		int seniors = in.nextInt();
-		System.out.print("Enter the number of adults: ");
-		int adults = in.nextInt();
-
-		System.out.println("The total cost of the tickets is $" + ((children*8)+(seniors*6)+(adults*16)));
-
-		in.close();
-	}
-}
-=======
 /*
  * Michael Pu
  * ICS3U1 - DataTypes
@@ -85,9 +12,9 @@ import java.util.Scanner;
 
 public class DataTypes {
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+
 		/*
 		 * QUESTION 1
 		 * INPUT: a number, N
@@ -97,7 +24,7 @@ public class DataTypes {
 		System.out.print("Enter a number: ");
 		double N = in.nextInt();
 		System.out.println("The cube of " + N + " is " + Math.pow(N, 3));
-		
+
 		/*
 		 * QUESTION 2
 		 * INPUT: a positive integer, N
@@ -107,7 +34,7 @@ public class DataTypes {
 		System.out.print("Enter a number: ");
 		N = in.nextInt();
 		System.out.println("The square root of " + N + " is " + Math.sqrt(N));
-		
+
 		/*
 		 * QUESTION 3
 		 * INPUT: integers a, b, c 
@@ -121,12 +48,12 @@ public class DataTypes {
 		System.out.print("Enter c: ");
 		int c = in.nextInt();
 
-		double thingUnderRoot = Math.sqrt((b*b) - (4*a*c));
-		double x1 = ((b*-1) + thingUnderRoot)/(2*a);
-		double x2 = ((b*-1) - thingUnderRoot)/(2*a);
+		double thingUnderRoot = Math.sqrt((b * b) - (4 * a * c));
+		double x1 = ((b * -1) + thingUnderRoot) / (2 * a);
+		double x2 = ((b * -1) - thingUnderRoot) / (2 * a);
 
 		System.out.println("The roots are " + x1 + " and " + x2);
-		
+
 		/*
 		 * QUESTION 4
 		 * INPUT: the number of children, seniors, and adults
@@ -140,9 +67,8 @@ public class DataTypes {
 		System.out.print("Enter the number of adults: ");
 		int adults = in.nextInt();
 
-		System.out.println("The total cost of the tickets is $" + ((children*8)+(seniors*6)+(adults*16)));
+		System.out.println("The total cost of the tickets is $" + ((children * 8) + (seniors * 6) + (adults * 16)));
 
 		in.close();
 	}
 }
->>>>>>> 82ddd5638917f1c881ce8c4d22d001ae0e9b00b0
--- src/intro/LoopExercises.java
+++ src/intro/LoopExercises.java
@@ -1,133 +1,3 @@
-<<<<<<< HEAD
-/*
- * Michael Pu
- * ICS3U1 - LoopExercises
- * November 2017
- */
-
-package intro;
-
-import java.util.Scanner;
-
-public class LoopExercises {
-
-    private static Scanner in;
-
-    public static void main(String[] args) {
-
-        in = new Scanner(System.in);
-
-        Question1();
-        Question2();
-        Question3();
-        Question4();
-        Question5();
-        Question6();
-    }
-
-    private static void Question1() {
-        /*-
-		 * QUESTION 1
-		 * INPUT: integers, start and end
-		 * OUTPUT: integers, from start to end
-		 */
-
-        System.out.print("Enter Start: ");
-        int start = in.nextInt();
-        System.out.print("Enter End: ");
-        int end = in.nextInt();
-
-        for (int i = start; i <= end; i++) {
-            System.out.println(i);
-        }
-        in.nextLine();
-    }
-
-    private static void Question2() {
-        /*-
-		 * QUESTION 2
-		 * INPUT: string, a word
-		 * OUTPUT: strings, the word as many times as there are letters in the word
-		 */
-
-        System.out.print("Enter a word: ");
-        String word = in.nextLine();
-        for (int i = 0; i < word.length(); i++) {
-            System.out.println(word);
-        }
-    }
-
-    private static void Question3() {
-        /*-
-		 * QUESTION 3
-		 * INPUT: strings, two words
-		 * OUTPUT: string, two words plus dots so that the length is 30
-		 */
-
-        System.out.print("Enter the first word: ");
-        String word1 = in.nextLine();
-        System.out.print("Enter the second word: ");
-        String word2 = in.nextLine();
-
-        System.out.print(word1);
-        for (int i = 0; i < (30 - word1.length() - word2.length()); i++) {
-            System.out.print(".");
-        }
-        System.out.println(word2);
-    }
-
-    private static void Question4() {
-        /*-
-		 * QUESTION 4
-		 * INPUT: a positive integer, a number N
-		 * OUTPUT: an integer, the sum of the first N natural numbers
-		 */
-
-        System.out.print("Enter N: ");
-        int n = in.nextInt();
-        int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            sum += i;
-        }
-        System.out.println("The sum is: " + sum);
-    }
-
-    private static void Question5() {
-        /*-
-		 * QUESTION 5
-		 * INPUT: a positive integer, a number N
-		 * OUTPUT: a double, the sum of the reciprocals of all numbers from 1 to N
-		 */
-
-        System.out.print("Enter N: ");
-        int n = in.nextInt();
-        double sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += (double) 1 / i;
-        }
-        System.out.println("The sum is: " + sum);
-    }
-
-    private static void Question6() {
-        /*-
-		 * QUESTION 6
-		 * INPUT: integer, the number of queries N; integers, the queries 
-		 * OUTPUT: a double, the sum of the reciprocals of all numbers from 1 to N
-		 */
-
-        System.out.print("Enter the number of queries: ");
-        int n = in.nextInt();
-        int x = 0;
-        int x2 = 0;
-        for (int i = 0; i < n; i++) {
-            int num = in.nextInt();
-            x += num;
-            x2 += num * num;
-        }
-        System.out.println("The standard deviation is " + Math.sqrt(((double) x2 / n) - Math.pow(((double) x / n), 2)));
-    }
-}
-=======
 /*
  * Michael Pu
  * ICS3U1 - LoopExercises
@@ -141,120 +11,119 @@ import java.util.Scanner;
 
 public class LoopExercises {
 
-    private static Scanner in;
+	private static Scanner in;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        in = new Scanner(System.in);
+		in = new Scanner(System.in);
 
-        Question1();
-        Question2();
-        Question3();
-        Question4();
-        Question5();
-        Question6();
-    }
+		Question1();
+		Question2();
+		Question3();
+		Question4();
+		Question5();
+		Question6();
+	}
 
-    private static void Question1() {
-        /*-
+	private static void Question1() {
+		/*-
 		 * QUESTION 1
 		 * INPUT: integers, start and end
 		 * OUTPUT: integers, from start to end
 		 */
 
-        System.out.print("Enter Start: ");
-        int start = in.nextInt();
-        System.out.print("Enter End: ");
-        int end = in.nextInt();
+		System.out.print("Enter Start: ");
+		int start = in.nextInt();
+		System.out.print("Enter End: ");
+		int end = in.nextInt();
 
-        for (int i = start; i <= end; i++) {
-            System.out.println(i);
-        }
-        in.nextLine();
-    }
+		for (int i = start; i <= end; i++) {
+			System.out.println(i);
+		}
+		in.nextLine();
+	}
 
-    private static void Question2() {
-        /*-
+	private static void Question2() {
+		/*-
 		 * QUESTION 2
 		 * INPUT: string, a word
 		 * OUTPUT: strings, the word as many times as there are letters in the word
 		 */
 
-        System.out.print("Enter a word: ");
-        String word = in.nextLine();
-        for (int i = 0; i < word.length(); i++) {
-            System.out.println(word);
-        }
-    }
+		System.out.print("Enter a word: ");
+		String word = in.nextLine();
+		for (int i = 0; i < word.length(); i++) {
+			System.out.println(word);
+		}
+	}
 
-    private static void Question3() {
-        /*-
+	private static void Question3() {
+		/*-
 		 * QUESTION 3
 		 * INPUT: strings, two words
 		 * OUTPUT: string, two words plus dots so that the length is 30
 		 */
 
-        System.out.print("Enter the first word: ");
-        String word1 = in.nextLine();
-        System.out.print("Enter the second word: ");
-        String word2 = in.nextLine();
+		System.out.print("Enter the first word: ");
+		String word1 = in.nextLine();
+		System.out.print("Enter the second word: ");
+		String word2 = in.nextLine();
 
-        System.out.print(word1);
-        for (int i = 0; i < (30 - word1.length() - word2.length()); i++) {
-            System.out.print(".");
-        }
-        System.out.println(word2);
-    }
+		System.out.print(word1);
+		for (int i = 0; i < (30 - word1.length() - word2.length()); i++) {
+			System.out.print(".");
+		}
+		System.out.println(word2);
+	}
 
-    private static void Question4() {
-        /*-
+	private static void Question4() {
+		/*-
 		 * QUESTION 4
 		 * INPUT: a positive integer, a number N
 		 * OUTPUT: an integer, the sum of the first N natural numbers
 		 */
 
-        System.out.print("Enter N: ");
-        int n = in.nextInt();
-        int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            sum += i;
-        }
-        System.out.println("The sum is: " + sum);
-    }
+		System.out.print("Enter N: ");
+		int n = in.nextInt();
+		int sum = 0;
+		for (int i = 0; i <= n; i++) {
+			sum += i;
+		}
+		System.out.println("The sum is: " + sum);
+	}
 
-    private static void Question5() {
-        /*-
+	private static void Question5() {
+		/*-
 		 * QUESTION 5
 		 * INPUT: a positive integer, a number N
 		 * OUTPUT: a double, the sum of the reciprocals of all numbers from 1 to N
 		 */
 
-        System.out.print("Enter N: ");
-        int n = in.nextInt();
-        double sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += (double) 1 / i;
-        }
-        System.out.println("The sum is: " + sum);
-    }
+		System.out.print("Enter N: ");
+		int n = in.nextInt();
+		double sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += (double) 1 / i;
+		}
+		System.out.println("The sum is: " + sum);
+	}
 
-    private static void Question6() {
-        /*-
+	private static void Question6() {
+		/*-
 		 * QUESTION 6
 		 * INPUT: integer, the number of queries N; integers, the queries 
 		 * OUTPUT: a double, the sum of the reciprocals of all numbers from 1 to N
 		 */
 
-        System.out.print("Enter the number of queries: ");
-        int n = in.nextInt();
-        int x = 0;
-        int x2 = 0;
-        for (int i = 0; i < n; i++) {
-            int num = in.nextInt();
-            x += num;
-            x2 += num * num;
-        }
-        System.out.println("The standard deviation is " + Math.sqrt(((double) x2 / n) - Math.pow(((double) x / n), 2)));
-    }
+		System.out.print("Enter the number of queries: ");
+		int n = in.nextInt();
+		int x = 0;
+		int x2 = 0;
+		for (int i = 0; i < n; i++) {
+			int num = in.nextInt();
+			x += num;
+			x2 += num * num;
+		}
+		System.out.println("The standard deviation is " + Math.sqrt(((double) x2 / n) - Math.pow(((double) x / n), 2)));
+	}
 }
->>>>>>> 82ddd5638917f1c881ce8c4d22d001ae0e9b00b0
